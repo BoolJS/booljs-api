fbf1047e14637a128c8715fb8232e8a2537f112a
Pablo Dorado <pandres95@boolinc.co>
2015-02-13 05:19:58 -0500

    Inserting components to API 1. Fix syntax issue 2. Test has been generalized

--- test/api.js
+++ test/api.js
@@ -1,9 +1,10 @@
 describe('API', function(){
 
-    it('Only have an error in API', function(done){
+    it('Listing of namespaces/classes into API must be as specified',
+    function(done){
 
         var apiKeys = Object.keys(API);
-        expect(apiKeys).to.eql(['Error']);
+        expect(apiKeys).to.eql(['Component', 'Error']);
         done();
 
     });
