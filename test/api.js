2f671d7b4c70df1061f700ab160f54bdd0edc6e4
Pablo Dorado <pandres95@boolinc.co>
2015-02-28 18:30:12 -0500

    1. Listing instances of applications by namespace 2. Added namespace checking and generating options 3. Added tests of listing and removing

--- test/api.js
+++ test/api.js
@@ -27,25 +27,10 @@ describe('API', function(){
     });
 
     it(
-        "App is a Singleton, it can't be instantiated as constructor, but " +
-        "an instance be gotten by `getInstance()`", function(done){
-
-            var App = API.Component.App;
-
-            var appInstance = App.getInstance()
-            ,   appInstance2 = App.getInstance();
-
-            expect(appInstance).to.eql(appInstance2);
-            done();
-
-        }
-    );
-
-    it(
         "App inherits BaseComponent, must implement its methods and " +
         "properties", function(done){
 
-            var appInstance = API.Component.App.getInstance();
+            var appInstance = API.Component.App.getInstance("com.boolinc.api");
 
             try{
                 appInstance.compatibleComponents();
@@ -56,5 +41,86 @@ describe('API', function(){
 
         }
     );
+
+    it(
+        "App namespace generator must check correctly bad namespaces and " +
+        "generate a skeleton for good ones", function(done){
+
+            var App = API.Component.App;
+
+            var badNamespaceApp, secondBadNamespace, thirdBadNamespace;
+            var anApp, aSecondApp, aThirdApp;
+
+            try {
+                badNamespaceApp = App.getInstance("my.bad-namespace.1app");
+                done(new Error("The namespace checker is failing"));
+            } catch(x){}
+
+            try {
+                secondBadNamespace = App.getInstance("my.bad_namespace.1app");
+                done(new Error("The namespace checker is failing"));
+            } catch(x){}
+
+            try {
+                thirdBadNamespace = App.getInstance("my.:bad_namespace.app2");
+                done(new Error("The namespace checker is failing"));
+            } catch(x){}
+
+            anApp = App.getInstance("com.boolinc.dogapi");
+            expect(anApp.getSkeleton()).to.eql({
+                com: {
+                    boolinc: {
+                        dogapi: {}
+                    }
+                }
+            });
+
+            aSecondApp = App.getInstance("com.bool_inc.dogapi");
+            expect(aSecondApp.getSkeleton()).to.eql({
+                com: {
+                    bool_inc: {
+                        dogapi: {}
+                    }
+                }
+            });
+
+            aThirdApp = App.getInstance("com.bool_inc.dog2api");
+            expect(aThirdApp.getSkeleton()).to.eql({
+                com: {
+                    bool_inc: {
+                        dog2api: {}
+                    }
+                }
+            });
+
+            done();
+
+        }
+    );
+
+    it("Applications created can be listed", function(done){
+
+        expect(API.Component.App.listInstances()).to.eql([
+            "com.boolinc.api",
+            "com.boolinc.dogapi",
+            "com.bool_inc.dogapi",
+            "com.bool_inc.dog2api"
+        ]);
+
+        done();
+
+    });
+
+    it("Applications created in pool can be removed", function(done){
+
+        var App = API.Component.App;
+
+        App.removeInstance("com.boolinc.dogapi");
+        App.removeInstance("com.bool_inc.dogapi");
+        App.removeInstance("com.bool_inc.dog2api");
+
+        done();
+
+    });
 
 });
